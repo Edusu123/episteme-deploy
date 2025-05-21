65e4cc2f46c35e56d43f0cd254cbb4442b366e15
Edusu123 <Edusu123@users.noreply.github.com>
2025-05-21 14:12:12 +0000

    deploy: c00p-dev/episteme-front@3d5f04f62f8ed7bce40939f1c6ed539e1f780177

--- app/api/auth/refresh/route.ts
+++ app/api/auth/refresh/route.ts
@@ -1,28 +1,28 @@
-import { cookies, headers } from "next/headers"
+import { cookies, headers } from 'next/headers';
 
 export async function POST(request: Request) {
-    const body = await request.json()
+  const body = await request.json();
 
-    const prefix = process.env.NODE_ENV === "development" ? "__Dev-" : ""
+  const prefix = process.env.NODE_ENV === 'development' ? '__Dev-' : '';
 
-    const cookieStore = await cookies()
-    const payload = {
-        refreshToken: cookieStore.get(`${prefix}dbp.refresh-token` as any)?.value,
-        userId: body.userID,
-    }
+  const cookieStore = await cookies();
+  const payload = {
+    refreshToken: cookieStore.get(`${prefix}dbp.refresh-token` as any)?.value,
+    userId: body.userID
+  };
 
-    // change it with your own endpoint
-    const res = await fetch(`${process.env.API_BASE_URL}/refresh`, {
-        method: "POST",
-        headers: await headers(),
-        body: JSON.stringify(payload)
-    })
+  // change it with your own endpoint
+  const res = await fetch(`${process.env.API_BASE_URL}/refresh-token`, {
+    method: 'POST',
+    headers: await headers(),
+    body: JSON.stringify(payload)
+  });
 
-    const data = await res.json()
+  const data = await res.json();
 
-    return Response.json({
-        success: res.ok,
-        status: res.status,
-        data,
-    })
+  return Response.json({
+    success: res.ok,
+    status: res.status,
+    data
+  });
 }
